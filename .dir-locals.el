aff8f74f945fdc61163accb3ea55b4f2be02f7fc
Xin Gu <eric.nevup@gmail.com>
2012-03-26 19:11:20 +0300

    Coding style for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+; suitable Emacs defaults for the HIPL coding style
+((nil . ((tab-width . 4)
+         (show-trailing-whitespace . t)))
+ (c-mode . ((c-basic-offset . 4)
+            (c-indentation-style . "k&r")))
+)

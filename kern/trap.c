3e52daac374b218e73bd1eef7a29f120c83d44b3
Xin Gu <eric.nevup@gmail.com>
2012-04-22 19:01:23 +0300

    Fix #6
    Build a global array "intr_addrs" to hold the addresses of handler functions,
    and the index of this array is the vector of the interrupt.
    The value "intr_len" indicates the size of the array.

--- kern/trap.c
+++ kern/trap.c
@@ -58,51 +58,21 @@ static const char *trapname(int trapno)
 	return "(unknown trap)";
 }
 
-extern void trap_divide(void);
-extern void trap_debug(void);
-extern void trap_nmi(void);
-extern void trap_breakpoint(void);
-extern void trap_overflow(void);
-extern void trap_bound(void);
-extern void trap_invalid_op(void);
-extern void trap_device(void);
-extern void trap_double_fault(void);
-extern void trap_tss(void);
-extern void trap_segment(void);
-extern void trap_stack(void);
-extern void trap_general_protection(void);
-extern void trap_page_fault(void);
-extern void trap_floating_point(void);
-extern void trap_alignment_check(void);
-extern void trap_machine_check(void);
-extern void trap_SIMD(void);
-
 void
 trap_init(void)
 {
 	extern struct Segdesc gdt[];
+	extern int intr_addrs[];
+	extern int intr_len;
 
-	// LAB 3: Your code here.
-	SETGATE(idt[T_DIVIDE], 0, GD_KT, (uintptr_t) trap_divide, 0);
-	SETGATE(idt[T_DEBUG], 0, GD_KT, (uintptr_t) trap_debug, 0);
-	SETGATE(idt[T_NMI], 0, GD_KT, (uintptr_t) trap_nmi, 0);
-	SETGATE(idt[T_BRKPT], 0, GD_KT, (uintptr_t) trap_breakpoint, 0);
-	SETGATE(idt[T_OFLOW], 0, GD_KT, (uintptr_t) trap_overflow, 0);
-	SETGATE(idt[T_BOUND], 0, GD_KT, (uintptr_t) trap_bound, 0);
-	SETGATE(idt[T_ILLOP], 0, GD_KT, (uintptr_t) trap_invalid_op, 0);
-	SETGATE(idt[T_DEVICE], 0, GD_KT, (uintptr_t) trap_device, 0);
-	SETGATE(idt[T_DBLFLT], 0, GD_KT, (uintptr_t) trap_double_fault, 0);
-	SETGATE(idt[T_TSS], 0, GD_KT, (uintptr_t) trap_tss, 0);
-	SETGATE(idt[T_SEGNP], 0, GD_KT, (uintptr_t) trap_segment, 0);
-	SETGATE(idt[T_STACK], 0, GD_KT, (uintptr_t) trap_stack, 0);
-	SETGATE(idt[T_GPFLT], 0, GD_KT, (uintptr_t) trap_general_protection, 0);
-	SETGATE(idt[T_PGFLT], 0, GD_KT, (uintptr_t) trap_page_fault, 0);
-	SETGATE(idt[T_FPERR], 0, GD_KT, (uintptr_t) trap_floating_point, 0);
-	SETGATE(idt[T_ALIGN], 0, GD_KT, (uintptr_t) trap_alignment_check, 0);
-	SETGATE(idt[T_MCHK], 0, GD_KT, (uintptr_t) trap_machine_check, 0);
-	SETGATE(idt[T_SIMDERR], 0, GD_KT, (uintptr_t) trap_SIMD, 0);
+	int i;
 
-	// Per-CPU setup 
+	// prepare IDT
+	for (i = 0; i < intr_len; i++) {
+		SETGATE(idt[i], 0, GD_KT, intr_addrs[i], 0);
+	}
+
+	// Per-CPU setup
 	trap_init_percpu();
 }
 
